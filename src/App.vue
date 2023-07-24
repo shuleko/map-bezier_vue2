9feffc0e6c3da3dcff934eb891ead9a41454da51
dshuleko <dshuleko@gallerymedia.onmicrosoft.com>
2023-07-24 11:25:24 +0300

    Начало

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="text-green-700 text-xl">Hello, from TailwindCSS!</div>
+    <map-bezier/>
+
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,16 +10,19 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    "map-bezier": Map
   }
 }
 </script>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import './styles/main.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -6,3 +7,4 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+S
